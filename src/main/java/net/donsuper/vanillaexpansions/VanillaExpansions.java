96ea6abd72ac52be2ad449119edda1692021acec
= <edwardsd2@wit.edu>
2024-08-14 12:36:55 -0400

    init

---
+++ src/main/java/net/donsuper/vanillaexpansions/VanillaExpansions.java
@@ -0,0 +1,16 @@
+package net.donsuper.vanillaexpansions;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class VanillaExpansions implements ModInitializer {
+	public static final String MOD_ID = "vanillaexpansions";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
